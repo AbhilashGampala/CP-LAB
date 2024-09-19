3cc1f045d645bb05acac97c4a9ed9744783d1e37
AbhilashGampala <abhilashgampala@gmail.com>
2024-09-19 16:18:43 +0530

    Add files via upload

---
+++ datasize.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int intType;
+	float floatType;
+	char charType;
+	printf("Size of int is %lu bytes\n",sizeof(intType));
+	printf("Size of float is %lu bytes\n",sizeof(floatType));
+	printf("Size of char is %lu sbytes\n",sizeof(charType));
+}
+
+
+
+
+
